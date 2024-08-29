df7ed7777701f322798c63f32b2f17502b71f8a7
harvey <harvey.d.brine@gmail.com>
2024-08-29 19:04:27 +1000

    put scraper into a function

--- python/src/main.py
+++ python/src/main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
+#Starting parameters
+
 chrome_options = Options()
 # chrome_options.add_argument("--disable-extensions")
 # chrome_options.add_argument("--disable-gpu")
@@ -10,9 +12,18 @@ chrome_options.add_argument("--headless=new") # for Chrome >= 109
 # chrome_options.add_argument("--headless")
 # chrome_options.headless = True # also works
 driver = webdriver.Chrome(options=chrome_options)
-start_url = "https://www.adobe.com/au/legal/licenses-terms.html"
-driver.get(start_url)
-time.sleep(5)
-print(driver.page_source.encode("utf-8"))
-# b'<!DOCTYPE html><html xmlns="http://www....
-driver.quit()
+
+#temporary test url
+temp_url = "https://www.adobe.com/au/legal/licenses-terms.html"
+
+
+#Scraper code
+def scrape(url):
+    driver.get(url)
+    time.sleep(5)
+    print(driver.page_source.encode("utf-8"))
+    # b'<!DOCTYPE html><html xmlns="http://www....
+    driver.quit()
+
+
+scrape(temp_url)
