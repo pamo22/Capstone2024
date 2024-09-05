37d0447c843ae0cbd508c6568b0b1c5a7763e679
harvey <harvey.d.brine@gmail.com>
2024-09-05 21:03:08 +1000

    testing module setup and working, need to implement tests for scraper, and comparison

--- python/src/test_scraper.py
+++ python/src/test_scraper.py
@@ -5,7 +5,7 @@
 
 #Imports
 import pytest
-from scraper import scrape
+import scraper
 
 
 #test url variable setting
@@ -31,33 +31,43 @@ test_url_list = [
 ]
 
 
-print(test_url_list)
+#print(test_url_list)
 #bytes strings
 #   expected ouput = eo
-eo1 = b""
+# this is the output of example.com
+eo1 = """<html><head>\n    <title>Example Domain</title>\n\n    <meta charset="utf-8">\n    <meta http-equiv="Content-type" content="text/html; charset=utf-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1">\n    <style type="text/css">\n    body {\n        background-color: #f0f0f2;\n        margin: 0;\n        padding: 0;\n        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;\n        \n    }\n    div {\n        width: 600px;\n        margin: 5em auto;\n        padding: 2em;\n        background-color: #fdfdff;\n        border-radius: 0.5em;\n        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);\n    }\n    a:link, a:visited {\n        color: #38488f;\n        text-decoration: none;\n    }\n    @media (max-width: 700px) {\n        div {\n            margin: 0 auto;\n            width: auto;\n        }\n    }\n    </style>    \n</head>\n\n<body>\n<div>\n    <h1>Example Domain</h1>\n    <p>This domain is for use in illustrative examples in documents. You may use this\n    domain in literature without prior coordination or asking for permission.</p>\n    <p><a href="https://www.iana.org/domains/example">More information...</a></p>\n</div>\n\n\n</body></html>""".encode()
 
 #run tests
+scraper1 = scraper.scrape_obj();
 
+eo1_scrapeout = scraper1.get_text("http://example.com")[0]
 
-
+print(eo1_scrapeout)
+print("-------------------------------------")
+print(eo1)
+print("-------------------------------------")
+print(eo1_scrapeout == eo1)
 #Test functions
 class TestScrape:
 
     #Access to internet
     def test_internet_access(self):
-        assert scrape("http://example.com") == expected_output
+        assert scraper1.get_text("http://example.com")[0] == eo1
 
+
+    #To be implemented:
+    """
     #Inproper input cases
     def test_url_bad_input(self):
-        assert scrape("") == expected_output
+        assert scraper1.get_text("") == 0#expected_output
 
     #Unexpected URL behaviour cases
     def test_url_unexpected_behaviour(self):
-        assert scrape("SOMETHING HERE") == expected_output
+        assert scraper1.get_text("SOMETHING HERE") == 0#expected_output
 
     #Known result test
     def test_scrape_result(self):
-        assert scrape("http://example.com") == expected_output
-
+        assert scraper1.get_text("http://example.com") == 0# expected_output
+    """
 
 
