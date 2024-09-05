bb3bb2d0174c1951fba62b3bd82d8fe61ed7ae07
Oscar <pamo@tuta.com>
2024-09-05 18:07:19 +1000

    making everything more object oriented

--- python/src/scraper.py
+++ python/src/scraper.py
@@ -8,7 +8,7 @@ class scrape_obj:
     driver = webdriver
     content = ""
     url = ""
-    def __init__(self, url):
+    def __init__(self):
         chrome_options = Options()
         chrome_options.add_argument("--headless=new") # for Chrome >= 109
         chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
@@ -25,7 +25,6 @@ class scrape_obj:
         "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
         })
         self.driver = webdriver.Chrome(options=chrome_options)
-        self.url = url
 
     def save_to_file(self, url:str, filename:str):
         self.driver.implicitly_wait(10)
