187e5d12c570c36e713b2e4a8f4c6376522251d6
Harvey <harvey.d.brine@gmail.com>
2024-09-06 11:37:32 +1000

    OOPed comparev2, updated tui to reflect changes

--- python/src/comparison_v2.py
+++ python/src/comparison_v2.py
@@ -1,21 +1,43 @@
 import timeit
 
-def comparison_cancel (file1, file2, outputname):
-    start = timeit.default_timer()
-    with open(file1, 'r') as fileA, open(file2, 'r') as fileB, open(outputname,"w") as output:
-        print("Comparing ",file1, " to ", file2, " exporting differences to ",outputname)
-        print("Differences detected: ")
-        linesA = fileA.readlines()
-        linesB = fileB.readlines()
+class compare_obj:
+    def __init__(self):
+        return None
 
-        diff_lista = [line_a for line_a in linesA if line_a not in linesB]
+    def compare_file(self, file1: str, file2: str) -> [str] :
+        with open(file1, 'r') as fileA, open(file2, 'r') as fileB:
+            linesA = fileA.readlines()
+            linesB = fileB.readlines()
 
-        for line in diff_lista:
-            print(line)
-            output.write(line)
+            diff_list = [line_a for line_a in linesA if line_a not in linesB]
+            return diff_list
 
 
-    stop = timeit.default_timer()
-    print('Comparison time: ', stop - start)
+    def compare_save_to_file(self, file1: str, file2: str, outputname: str):
+        outputlist = self.compare_file(file1, file2)
 
+        with open(outputname, 'w') as outputFile:
+            for line in outputlist:
+                outputFile.write(line)
+
+
+    def compare_file_verbose(self, file1: str, file2: str):
+            start = timeit.default_timer()
+
+            with open(file1, 'r') as fileA, open(file2, 'r') as fileB:
+                print("Comparing ",file1, " to ", file2)
+                print("Differences detected: ")
+                linesA = fileA.readlines()
+                linesB = fileB.readlines()
+
+                diff_list = [line_a for line_a in linesA if line_a not in linesB]
+
+                print("Differences found: ")
+                for line in diff_list:
+                    print(line)
+
+                return diff_list
+
+            stop = timeit.default_timer()
+            print('Comparison time: ', stop - start)
 
--- python/src/tui.py
+++ python/src/tui.py
@@ -1,6 +1,6 @@
 import os
 from scraper import scrape_obj
-from comparison_v2 import comparison_cancel
+from comparison_v2 import compare_obj
 from dbinterface import dbinterface
 import uuid
 
@@ -20,10 +20,11 @@ def menu_options():
     print("1. Scrape custom url")
     print("2. Run scrape tests") # idk if pytest likes being called from here
     print("3. Compare 2 files")
-    print("4. Run compare tests") # idk if pytest likes being called from here
+    print("4. Compare 2 files and save to file") # idk if pytest likes being called from here
     print("5. Scrape url and add to database")
     print("6. Scrape url and save to file")
     scraperhandle = scrape_obj()
+    comparehandle = compare_obj()
 
     match input():
         case '1':
@@ -35,16 +36,24 @@ def menu_options():
             #temp file names
             file1 = "src/eg_1.txt"
             file2 = "src/eg_2.txt"
-            comparison_cancel(file1, file2, "comparison_output.txt")
+            comparehandle.compare_file_verbose(file1, file2)
+            comparehandle.compare_file_verbose(file2, file1)
+        case '4':
+            print("You pressed 4")
+            #temp file names
+            file1 = "src/eg_1.txt"
+            file2 = "src/eg_2.txt"
+            comparehandle.compare_save_to_file(file1, file2,"F1compF2_differences.txt")
+            comparehandle.compare_save_to_file(file2, file1,"F2compF1_differences.txt")
         case '5':
             print("You pressed 5")
             url = input("Input url to be scraped: ")
-            title = input("Plesae input a name to refer to this licence by: ")
+            title = input("Please input a name to refer to this licence by: ")
             filename = uuid.uuid4()
             scraped_text = scraperhandle.get_text(url)
             file_result = scraperhandle.save_to_file(url, str(filename))
 
-            
+
             #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             # PLAINTEXT CREDENTIALS IN SOURCE CODE || TESTING ONLY
             mongohandle = dbinterface("mongodb://mytester2:databased1204@localhost:27017")
