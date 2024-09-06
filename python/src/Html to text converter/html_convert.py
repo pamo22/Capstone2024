a456da486f205735bbdd2a26d13140e41200d2ef
PeanutFlips <benedikt.matthews@gmail.com>
2024-09-06 13:35:27 +1000

    Html to text converter
    Made some improvement to the html_to_text converter and tested them on the Oracle.html license

--- python/src/Html to text converter/html_convert.py
+++ python/src/Html to text converter/html_convert.py
@@ -1,14 +1,15 @@
 # Html to Text converter
-# Takes a html file input and returns a txt file input,
+# Function takes a html file input and returns a txt file input,
 # processed appropriately for compare text function.
 # Author: Benedikt Matthews
-# Last Updated: 03/09/2024
+# Last Updated: 06/09/2024
 
 
 # importing the library
 from bs4 import BeautifulSoup
 
-from python.src.scraper import scrape
+from python.src.scraper import scrape_obj
+
 
 # Conversion Function, takes a local html file
 # and converts the document into more readable text
@@ -18,12 +19,24 @@ def html_converter(html_file):
     file = open(html_file, "r", encoding="utf-8")
     # Uses BeautifulSoup to output the raw content from the html file
     raw = BeautifulSoup(file, "html.parser")
+
+    # Rips out button text, which should practically never be relevant
+    # Can add more tags here if necessary
+    for script in raw(["button"]):
+        script.extract()
+
     # Formats the soup object into a properly nested structure
     formatted = (raw.prettify(encoding="utf-8"))
     # Creates another soup object from the prettified content
     output = BeautifulSoup(formatted, "html.parser")
     # Retrieves the text from the output soup object
     text = output.get_text()
+
+    # Breaks text into lines and removes trailing gaps
+    lines = (line.strip() for line in text.splitlines())
+
+    # Drop blank lines
+    text = '\n'.join(line for line in lines if line)
 
     return text
 
@@ -39,19 +52,16 @@ def writefile(name, text):
     txtfile.close()
 
 
-# Define example variable here
+# Test URL
+url = "https://www.oracle.com/downloads/licenses/no-fee-license.html"
+# fLicense = "Adobe General Terms of Use.html"
+nLicense = "Oracle"
 
-url = "https://www.adobe.com/au/legal/terms.html"
-#fLicense = "Adobe General Terms of Use.html"
-nLicense = "Adobe"
-
-# Testing with scraper and defined url
-scrape(url)
+# Testing with scraper and defined url, CURRENTLY NOT WORKING
+# scrape(url)
 
 # Convert html file to text
 lText = html_converter("out.html")
 
 # Write text to file with appropriate name
 writefile(nLicense, lText)
-
-
