35772da60bbe1172c1beb703d66b8aeff804aeef
Oscar <pamo@tuta.com>
2024-09-18 10:43:21 +1000

    autorun thing works? still gotta test it

--- python/src/autorundaemon.py
+++ python/src/autorundaemon.py
@@ -1,63 +1,85 @@
 import pymongo
 from bson import ObjectId
-from bson.errors import InvalidId
 from datetime import datetime
 import time
-import uuid
 from scraper import scrape_obj
 from comparison_v2 import compare_obj
 
-
-#    def _now_millis(self):
-#        return round(time.time() * 1000)
-#
-#    def _save_license_info(self, title: str, url: str, ref_id):
-#        generated_filename = str(uuid.uuid4())
-#        self.shandle.get_text(url)
-#        scraped_text = self.shandle.save_to_file(url, generated_filename)
-#        create_result = self.db.licenses.insert_one(
-#            {
-#                "title": title,
-#                "created_date": self._now_millis(),
-#                "url": url,
-#                "filepath": scraped_text[0],
-#                "filetype": self.shandle.filetype,
-#                "content": self.shandle.content,
-#                "file_ref_uuid": generated_filename,
-#                "content_checksum": self.c_obj.checksum_bytes(self.shandle.content),
-#                "tracker_ref_id": ref_id
-#             }
-#        )
-#        print(f"New Todo ID: {create_result.inserted_id}")
-#        return None
-#    def _check_license_changed(self, url) -> bool:
-#        if (self.shandle.content == ""):
-#            print("gettign congteht")
-#            self.shandle.get_text(url)
-#        old_content_checksum = self.db.licenses.find_one(sort=[('_id', -1)]).get('content_checksum')
-#        return self.c_obj.compare_bytes_checksum(self.shandle.content, old_content_checksum)
-#    
-#    def add_license(self, title: str, url: str, frequency: int):
-#        #frequency should be in hours, we store time in millis so convert it
-#
-#        frequency = int(frequency) * 3600000
-#        ref_id = ObjectId()
-#        self._save_license_info(title, url, ref_id)
-#        self.db.tracker.insert_one(
-#                    {
-#                        "_id": ref_id,
-#                        "title": title,
-#                        "url": url,
-#                        "frequency": frequency,
-#                        "last_checked": self._now_millis()
-#                     }
-#                )
-#
-#    #def fetch_license(
-
-shandle = scrape_obj()
-db_client = pymongo.MongoClient("mongodb://mytester2:databased1204@localhost:27017");
+# Initialize database connection and objects
+db_client = pymongo.MongoClient("mongodb://mytester2:databased1204@localhost:27017")
 db = db_client["testdb"]
-c_obj = compare_obj()
-while True:
-        
+scrape_handle = scrape_obj()
+compare_handle = compare_obj()
+
+def _now_millis():
+    return round(time.time() * 1000)
+
+def update_tracker():
+    """Checks and updates items in the tracker database collection."""
+    now_millis = _now_millis()
+    tracker_collection = db.tracker
+    licenses_collection = db.licenses
+    
+    # Fetch all items from the tracker collection
+    items = tracker_collection.find()
+
+    for item in items:
+        last_checked = item.get('last_checked', 0)
+        frequency = item.get('frequency', 0)
+        url = item.get('url', '')
+        ref_id = item.get('_id')
+
+        # Check if it's time to check this item
+        if now_millis - last_checked >= frequency:
+            print(f"Checking item: {item['title']}")
+
+            # Fetch current content
+            scrape_handle.get_text(url)
+            new_content = scrape_handle.content
+            new_checksum = compare_handle.checksum_bytes(new_content)
+            
+            # Fetch old content from the database
+            old_license = licenses_collection.find_one({'tracker_ref_id': ref_id})
+            old_checksum = old_license.get('content_checksum') if old_license else None
+            
+            if new_checksum != old_checksum:
+                print(f"Content has changed for {item['title']}. Updating database...")
+                # Save new license info to the database
+                generated_filename = str(uuid.uuid4())
+                filepath = scrape_handle.save_to_file(url, generated_filename)
+                
+                licenses_collection.insert_one(
+                    {
+                        "title": item['title'],
+                        "created_date": now_millis,
+                        "url": url,
+                        "filepath": filepath[0],
+                        "filetype": scrape_handle.filetype,
+                        "content": new_content,
+                        "file_ref_uuid": generated_filename,
+                        "content_checksum": new_checksum,
+                        "tracker_ref_id": ref_id,
+                        "changes": compare_handle.compare_bytes(new_content, old_license)
+                    }
+                )
+                
+                
+                # Update last_checked time in the tracker collection
+                tracker_collection.update_one(
+                    {'_id': ref_id},
+                    {'$set': {'last_checked': now_millis}}
+                )
+            else:
+                print(f"No changes detected for {item['title']}.")
+                tracker_collection.update_one(
+                    {'_id': ref_id},
+                    {'$set': {'last_checked': now_millis}}
+                )
+    
+    print("Tracker update complete.")
+
+# To run the update function periodically, you might use a loop or a scheduler
+if __name__ == "__main__":
+    while True:
+        update_tracker()
+        time.sleep(5)  # Check every hour
